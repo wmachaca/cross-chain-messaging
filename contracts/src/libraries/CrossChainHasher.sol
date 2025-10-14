89b9bb5e61222e1df5594b584d6f1debd3169338
William I <william.machaca@hotmail.com>
2025-10-14 16:16:18 -0300

    feat: add CrossChainHasher library for computing game move Merkle leaves/LIN

---
+++ contracts/src/libraries/CrossChainHasher.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+library CrossChainHasher {
+    function computeGameMoveLeaf(
+        uint256 chainId,
+        address contractAddress,
+        address player,
+        uint8 move,
+        uint256 blockNumber,
+        uint256 balance,
+        uint256 eventId 
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(
+            chainId, contractAddress, player, move, blockNumber, balance, eventId 
+        ));
+    }
+}
